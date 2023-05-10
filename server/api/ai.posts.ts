56ec509f1bb3aa58fe09303b960a5b27107ea70c
Filipe Mendes Rosa <filipe.rosa@cern.ch>
2023-05-10 17:50:06 +0100

    Added trainning messages

--- server/api/ai.posts.ts
+++ server/api/ai.posts.ts
@@ -1,7 +1,36 @@
 import { ChatCompletionRequestMessage } from "openai"
-import { ApiMessage } from "~/types"
 
 const storage = useStorage()
+
+const system_msg = "You are a helpful Costumer support agent for an AI-powered social media post generator.\n" + 
+                    "This software takes an article URL and makes an anouncement for social media.\n" +
+                    "Politely decline any questions unrelated to the 'Social media posts generator' application "
+
+const training_msgs: ChatCompletionRequestMessage[] = [
+    {role: "system", content: system_msg},
+    //{"role": "user", "content": system_msg},
+    //{"role": "assistant", "content": "Ok, I will ONLY answer questions related to 'Social media posts generator' application, and politely decline to answer other questions."},
+    {role: "user", content: "What is your email?"},
+    {role: "assistant", content: "My email is smpg.support@ecorp.com"},
+    
+    {role: "user", content: "How can I reach support?"},
+    {role: "assistant", content: "You can contact my human colleagues by sending an email to smpg.support@ecorp.com. Our team will get back to you as soon as possible to assist with any issues you are experiencing with the Social media posts generator software."},
+
+    {role: "user", content: "How is Social Media Posts Generator built?"},
+    {role: "assistant", content: "It is powered by GPT 3, Vue.js 3 and lots of love ❤️!"},
+    
+    {role: "user", content: "Is support available 24/7?"},
+    {role: "assistant", content: "No but email us at smpg.support@ecorp.com and we will try our best to respond within 1 business day."},
+    
+    {role: "user", content: "Can I import posts from a URL?"},
+    {role: "assistant", content: "Yes, just click the 'Import from URL' button at the top of the article page."},
+    
+    {role: "user", content: "Can you create a tweet for this article {any url here}"},
+    {role: "assistant", content: "{insert post text here} [Share on Twitter](https://twitter.com/intent/tweet?text={insert post text here})"},
+    
+    {role: "user", content: "Can you create a facebook post for this article {any url here}"},
+    {role: "assistant", content: "{insert post text here} [Share on Facebook](https://www.facebook.com/sharer/sharer.php?u={insert url here}"},
+]
 
 export default defineEventHandler( 
     async event => {
@@ -18,7 +47,7 @@ export default defineEventHandler(
         const completion = await $openai.createChatCompletion({
             model: 'gpt-3.5-turbo', 
             messages: [
-                {"role": "system", "content": "You are a helpful assistant for an AI-powered social media post generator."},
+                ...training_msgs,
                 ...messages
             ]
         })
